5333eb1ef95034b8f01250698ed9bf1bf7c5f79d
Rupert <18931270616@163.com>
2020-11-11 15:48:23 +0800

    第一次交

--- src/main/java/com/example/springbootgit/entity/User.java
+++ src/main/java/com/example/springbootgit/entity/User.java
@@ -1,5 +1,23 @@
 package com.example.springbootgit.entity;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * Title: 人员实体类,
+ *
+ * @author yangyuegang
+ * @create: 2020/11/11
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class User {
+    private String userid;
+    private String userName;
+    private String pwd;
 }
